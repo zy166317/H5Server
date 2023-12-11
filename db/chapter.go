690316a053ef5bacec1d88e17acaf95b8c368487
yu <728505325@qq.com>
2023-12-11 17:15:13 +0800

    update

--- db/chapter.go
+++ db/chapter.go
@@ -1,14 +1,14 @@
 package db
 
-import "time"
+import (
+	"gorm.io/gorm"
+)
 
 type Chapter struct {
-	Id        int64     `gorm:"column:id;type:bigint(20);primaryKey;unique;AUTO_INCREMENT"`
-	Pos       string    `gorm:"column:pos;type:text"`
-	Color     string    `gorm:"column:color;type:text"`
-	Types     string    `gorm:"column:types;type:text"`
-	CreatedAt time.Time `gorm:"column:created_at;type:datetime(3)"`
-	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime(3)"`
+	gorm.Model
+	Pos   string `gorm:"column:pos;type:text"`
+	Color string `gorm:"column:color;type:text"`
+	Types string `gorm:"column:types;type:text"`
 }
 
 func (c *Chapter) TableName() string {
